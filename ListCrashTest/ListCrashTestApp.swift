85208598b79b676e0fb41ccc34b10b8d41c265ee
Konstantin Malyshev <ruzambo@gmail.com>
2021-01-09 03:50:16 +0200

    Initial Commit

---
+++ ListCrashTest/ListCrashTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListCrashTestApp.swift
+//  ListCrashTest
+//
+//  Created by Konstantin Malyshev on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ListCrashTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
